8a242715790d8722d85928151da1dfdf31d76195
Diana <ddiaz@udel.edu>
2024-02-15 14:42:23 -0500

    task3

--- src/App.tsx
+++ src/App.tsx
@@ -17,24 +17,27 @@ function App(): JSX.Element {
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript DIANA DIAZ
             </header>
-
+            <>
+                <div>
+                    <Button onClick={() => console.log("Hello World!")}>
+                        Log Hello World
+                    </Button>
+                </div>
+            </>
             <div>
-                <Button onClick={() => console.log("Hello World!")}>
-                    Log Hello World
-                </Button>
+                <Container>
+                    <Row>
+                        <Col> First col is here </Col>
+                        <Col>
+                            {" "}
+                            <img
+                                src="https://www.discoverpuertorico.com/sites/default/files/styles/horizontal_narrow_800x600/public/2019-02/playuela-beach-cabo-rojo.jpg?h=6eb229a4&itok=H9UWuO1C"
+                                style={{ width: "120px", height: "100px" }}
+                            />{" "}
+                        </Col>
+                    </Row>
+                </Container>
             </div>
-            <Container>
-                <Row>
-                    <Col> First col is here </Col>
-                    <Col>
-                        {" "}
-                        <img
-                            src="https://www.discoverpuertorico.com/sites/default/files/styles/horizontal_narrow_800x600/public/2019-02/playuela-beach-cabo-rojo.jpg?h=6eb229a4&itok=H9UWuO1C"
-                            style={{ width: "120px", height: "100px" }}
-                        />{" "}
-                    </Col>
-                </Row>
-            </Container>
             <div style={{ border: "1px solid red", padding: "4px" }}>
                 <ol>
                     <li>Palm Trees</li>
