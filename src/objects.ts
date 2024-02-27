88b55d114aaa8c26dcfe23c90950f83be3083485
Diana <ddiaz@udel.edu>
2024-02-26 20:04:28 -0500

    task6

--- src/objects.ts
+++ src/objects.ts
@@ -45,10 +45,10 @@ export function isCorrect(question: Question, answer: string): boolean {
 export function isValid(question: Question, answer: string): boolean {
     if (question.type === "short_answer_question") {
         return true;
-    }
-    if (question.type === "multiple_choice_question") {
+    } else if (question.type === "multiple_choice_question") {
         return question.options.includes(answer);
     }
+    return false;
 }
 
 /**
